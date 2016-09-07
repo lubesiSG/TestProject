174a18c081fd394b7e78551c6ce1ab4f750c5a4a
Zdeněk Novák <zdenek.novak@stonehengegames.com>
2016-09-07 17:07:14 +0200

    Added game core

---
+++ Assets/Scripts/GameCore.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameCore : MonoBehaviour
+{
+    public void Awake()
+    {
+        Debug.Log("GameCore awake");
+    }
+
+    public void Start()
+    {
+        Debug.Log("GameCore start");
+    }
+}
